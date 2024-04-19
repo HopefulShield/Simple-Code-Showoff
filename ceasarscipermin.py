498237446c7527cc10787a6aef43c49b956c5b19
HopefulShield <tranbryanm@gmail.com>
2024-04-18 23:48:13 -0700

    Minor updates: Cipher has comments, Fibo functionality update

--- ceasarscipermin.py
+++ ceasarscipermin.py
@@ -1,34 +1,27 @@
-#get string 
-#get cipher shift number 
-#make string lowercase 
-#make string to list form 
-#iterate through list string and shift all alpha chars by cipher shift 
-#   make sure alpha chars remain alpha char, with modulo
-#return string as "".join(list string)
 #test string "ABCdefghijklmnopqrstuvwxyz1%" 
 
 import sys
 
 cipher = "a"
 while not cipher.isdigit():
-    cipher = input("Please input an integer for the cipher shift: ") 
+    cipher = input("Please input an integer for the cipher shift: ") #get cipher shift number 
 #print(cipher.isdigit())
 cipher = int(cipher)
 
-userString = input("Please input a string: ") 
+userString = input("Please input a string: ") #get string 
 print("User input string:", userString)
-userString = userString.lower() 
-userString = list(userString) 
+userString = userString.lower() #make string lowercase 
+userString = list(userString) #make string to list form 
 
-for i in range(len(userString)): 
+for i in range(len(userString)): #iterate through list string and shift all alpha chars by cipher shift 
     #print(var[i], "isalpha?:", var[i].isalpha())
-    if userString[i].isalpha(): 
+    if userString[i].isalpha(): #   make sure alpha chars remain alpha char, with modulo
         shifted = ord(userString[i]) + cipher 
         shifted = ((shifted - 97) % 26) + 97
         userString[i] = chr(shifted)
         #print(var[i])
 
-print("En/De-crypted input string: ", "".join(userString)) 
+print("En/De-crypted input string: ", "".join(userString)) #return string as "".join(list string)
 
 
 
--- fibonacchi.py
+++ fibonacchi.py
@@ -1,9 +1,27 @@
-print("printing first 10 or 11 of fibonachi")
+import sys
+
+fibo = [0, 1]
+curr_I = 2
+
+
+
+
+fibo_n = "a"
+while not fibo_n.isdigit():
+    fibo_n = input("Please input an integer for the cipher shift: ") #get cipher shift number 
+#print(cipher.isdigit())
+fibo_n = int(fibo_n)
+fibo_n = 10
 
 i0 = 0 
 i1 = 1
-for i in range(10): 
-    new = i0 + i1 
-    i0 = i1 
-    i1 = new 
+for i in range(fibo_n): 
+    nM2 = curr_I - 2 
+    nM1 = curr_I - 1 
+
+    new = fibo[nM1] + fibo[nM2]
+    fibo.append(new)
+    curr_I += 1
     print(new)
+
+print(fibo)
